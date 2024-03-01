0b13bcd06001893289976b3886fbd82590a6da66
Alvan Wazecky <alvan55zecky@gmail.com>
2024-03-01 20:14:39 +0530

    Updated .gitignore

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$servername = "j1r4n2ztuwm0bhh5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-$username = "d30xlnm0y02a823r";
-$password = "qha1s1wzfemt6cyg"; 
-$database = "cs27gnq2fixwa61l";
+$servername = "localhost";
+$username = "root";
+$password = ""; 
+$database = "booking-ticket";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $database);
