84af5b57e59c888300c95b2c72a914db7744f06b
Alvan Wazecky <alvan55zecky@gmail.com>
2024-03-01 18:22:23 +0530

    Modified DB Connection

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,12 +1,12 @@
 <?php
-// Database configuration
-$servername = "localhost";
-$username = "root";
-$password = ""; 
-$database = "booking-ticket";
+// Heroku database configuration
+$host = getenv('DB_HOST');
+$username = getenv('DB_USERNAME');
+$password = getenv('DB_PASSWORD');
+$database = getenv('DB_DATABASE');
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysqli($host, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
